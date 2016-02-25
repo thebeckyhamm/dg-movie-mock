6463e1274b7324298d4721f58f5f2f5a2fcdca00
Becky Hamm <becky@metricslice.com>
2016-02-25 13:23:34 -0500

    split out list view and add ui router

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
-var dgMovieApp = angular.module('dgMovieApp', []);
+var dgMovieApp = 
+    angular
+        .module('dgMovieApp', [
+            'ui.router'
+            ])
+
+        .config(function($stateProvider, $urlRouterProvider) {
+            
+            $urlRouterProvider.otherwise('/');
+            
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'list/list.html',
+                    controller: 'ListController',
+                    controllerAs: 'vm'
+                })
+                
+        });
+
