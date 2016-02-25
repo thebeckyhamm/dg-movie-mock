5e780aa07ec30b4a37f4da806e17484701c59077
Becky Hamm <becky@metricslice.com>
2016-02-25 17:49:56 -0500

    get toggle buttons working

--- list/list.controller.js
+++ list/list.controller.js
@@ -8,28 +8,69 @@
 
 
     function ListController(movieIDs, movieDetails) {
-        var vm = this;
+        var vm  = this,
+            IDs = [];
         vm.movies = [];
-        vm.movieIDs = [];
+        vm.filteredMovies = [];
+        vm.toggleFilter = toggleFilter;
+        vm.toggle = 2000;
 
 
         function getMovies() {
 
-            var IDs = movieIDs.getMovieIDs();
+            var IDsService = movieIDs.getMovieIDs();
 
-            IDs
+            IDsService
                 .then(function (result) {
-                    vm.movieIDs = result;
+                    IDs = result;
 
                     return movieDetails.getMovieDetails(result)
                 }).
                 then( function( result) {
                     console.log(result);
                     vm.movies = result;
+                    toggleFilter(2000);
                 })
         }
 
         getMovies();
+
+        function filterMovies() {
+            _.each(vm.movies, function(m, i) {
+                if ( parseInt(m.Year) >= 2000 ) {
+
+                    vm.filteredMovies.push(m);
+
+                }
+            });
+            console.log(vm.filteredMovies);
+        }
+
+        function toggleFilter(year) {
+            vm.filteredMovies = [];
+
+            _.each(vm.movies, function(m, i) {
+                if (year === 2000) {
+                    if ( parseInt(m.Year) >= 2000 ) {
+
+                        vm.filteredMovies.push(m);
+
+                    }
+                    vm.toggle = 2000;
+                }
+                else {
+
+                    if ( parseInt(m.Year) >= 1990 && parseInt(m.Year) < 2000 ) {
+
+                        vm.filteredMovies.push(m);
+
+                    }
+                    vm.toggle = 1990;
+                }
+
+            });
+
+        }
 }
 
 
